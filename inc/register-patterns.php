f03c588671457171a687c26c21baa8a17aa6bef2
Alfredo Navas <elpuas@gmail.com>
2024-08-01 21:14:35 -0600

    On branch feature/home-patterns  Changes to be committed: 	modified:   inc/register-patterns.php 	new file:   patterns/headline.php 	new file:   src/css/globals/globals.css 	renamed:    src/patterns/go-excerpt.css -> src/css/patterns/go-excerpt.css 	renamed:    src/patterns/go-footer.css -> src/css/patterns/go-footer.css

--- inc/register-patterns.php
+++ inc/register-patterns.php
@@ -18,7 +18,7 @@ function gomarketing_register_patterns() {
         'gomarketing/header-default', 
         [
             'title'       => __( 'Header Default', 'gomarketing' ),
-            'categories'  => ['header', 'hero'],
+            'categories'  => ['header', 'gomarketing/go-patterns'],
             'source'      => 'theme',
             'description' => __( 'A hero section with navigation, a paragraph, a CTA button, and a background video.', 'gomarketing' ),
             'content'     => include get_stylesheet_directory() . '/patterns/header-default.php',
@@ -30,14 +30,37 @@ function gomarketing_register_patterns() {
         'gomarketing/footer-default', 
         [
             'title'       => __( 'Footer Default', 'gomarketing' ),
-            'categories'  => ['footer', 'hero'],
+            'categories'  => ['footer', 'gomarketing/go-patterns'],
             'source'      => 'theme',
             'description' => __( 'The Go Footer.', 'gomarketing' ),
             'content'     => include get_stylesheet_directory() . '/patterns/footer-default.php',
             'blockTypes'  => ['core/template-part/header']
         ]
     );
+
+    register_block_pattern( 
+        'gomarketing/headline', 
+        [
+            'title'       => __( 'Headline', 'gomarketing' ),
+            'categories'  => ['banner', 'featured', 'gomarketing/go-patterns'],
+            'source'      => 'theme',
+            'description' => __( 'The Go Headline.', 'gomarketing' ),
+            'content'     => include get_stylesheet_directory() . '/patterns/headline.php',
+            'blockTypes'  => ['core/group', 'core/post-content']
+        ]
+    );
 }
 
 // Register the block pattern on init
 add_action( 'init', __NAMESPACE__ . '\gomarketing_register_patterns' );
+
+function register_pattern_categories() {
+	register_block_pattern_category( 
+        'gomarketing/go-patterns', 
+        [
+            'label'       => __( 'Go Patterns', 'gomarketing' ),
+            'description' => __( 'A Collection of Go Patterns.', 'gomarketing' )
+	] );
+}
+
+add_action( 'init', __NAMESPACE__ . '\register_pattern_categories' );
