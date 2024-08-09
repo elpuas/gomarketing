574af9846b85055fde583c98b552096d4c3a8c9d
Alfredo Navas <elpuas@gmail.com>
2024-08-08 19:08:19 -0600

    On branch feature/home-patterns  Changes to be committed: 	modified:   patterns/headline-with-video.php 	modified:   src/css/globals/globals.css 	new file:   src/css/patterns/go-carousel.css 	modified:   src/styles.css

--- patterns/headline-with-video.php
+++ patterns/headline-with-video.php
@@ -6,29 +6,47 @@
  */
 
 return <<<EOD
-<!-- wp:group {"tagName":"section","metadata":{"name":"Headline with Video"},"align":"full","className":"gradient-bg-gw go-video","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<section class="wp-block-group alignfull gradient-bg-gw go-video" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"metadata":{"name":"Video Wrapper"},"className":"go-video-wrapper","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained","contentSize":""}} -->
-<div class="wp-block-group go-video-wrapper"><!-- wp:group {"metadata":{"name":"Headline"},"align":"wide","className":"go-video-headline","style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"tagName":"section","metadata":{"name":"Partners Carousel"},"align":"full","className":"go-brands-carousel","style":{"color":{"background":"#f7f7f7"},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull go-brands-carousel has-background" style="background-color:#f7f7f7;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-bottom:0"><!-- wp:group {"metadata":{"name":"Headline"},"align":"wide","className":"go-video-headline","style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide go-video-headline"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
-<p class="has-text-align-center has-large-font-size">El equipo de Go!</p>
+<p class="has-text-align-center has-large-font-size">Nuestros clientes</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"45px","fontStyle":"normal","fontWeight":"700","textTransform":"uppercase"}},"fontFamily":"montserrat"} -->
-<h2 class="wp-block-heading has-text-align-center has-montserrat-font-family" style="font-size:45px;font-style:normal;font-weight:700;text-transform:uppercase">Lleva tu marca a lo <br>más alto.</h2>
+<h2 class="wp-block-heading has-text-align-center has-montserrat-font-family" style="font-size:45px;font-style:normal;font-weight:700;text-transform:uppercase">los que confían en nuestro equipo</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"fontSize":"large"} -->
-<p class="has-text-align-center has-large-font-size" style="margin-bottom:var(--wp--preset--spacing--30);line-height:1">Nos especializamos en crear experiencias que destacan y conectan. <br>Transformamos tus ideas en eventos únicos y memorables que capturan la esencia de tu marca.</p>
+<p class="has-text-align-center has-large-font-size" style="margin-bottom:var(--wp--preset--spacing--30);line-height:1">En Go! Marketing nos sentimos orgullosos de trabajar con marcas y personas que valoran la excelencia y la creatividad.</p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"metadata":{"name":"Video"},"align":"wide","className":"go-video-container","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide go-video-container"><!-- wp:embed {"url":"https://www.youtube.com/watch?v=u31qwQUeGuM","type":"video","providerNameSlug":"youtube","responsive":true,"align":"wide","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure class="wp-block-embed alignwide is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-https://www.youtube.com/watch?v=u31qwQUeGuM
-</div></figure>
-<!-- /wp:embed --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></section>
+<!-- wp:cb/carousel {"slidesToScroll":3,"arrows":false,"autoplay":true,"autoplaySpeed":2000,"infinite":true,"responsiveWidth":840,"responsiveSlides":2,"responsiveSlidesToScroll":2,"align":"wide","className":"go-carousel"} -->
+<div class="wp-block-cb-carousel alignwide go-carousel" data-slick="{&quot;slidesToShow&quot;:3,&quot;slidesToScroll&quot;:3,&quot;speed&quot;:300,&quot;arrows&quot;:false,&quot;dots&quot;:true,&quot;autoplay&quot;:true,&quot;autoplaySpeed&quot;:2000,&quot;infinite&quot;:true,&quot;responsive&quot;:[{&quot;breakpoint&quot;:841,&quot;settings&quot;:{&quot;slidesToShow&quot;:2,&quot;slidesToScroll&quot;:2}}]}"><!-- wp:cb/slide {"className":"go-carousel-slide"} -->
+<div class="wp-block-cb-slide go-carousel-slide"><!-- wp:image {"id":138,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://gomarketing.local/wp-content/uploads/2024/08/Group-23-jpg.avif" alt="" class="wp-image-138"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:cb/slide -->
+
+<!-- wp:cb/slide {"className":"go-carousel-slide"} -->
+<div class="wp-block-cb-slide go-carousel-slide"><!-- wp:image {"id":139,"scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://gomarketing.local/wp-content/uploads/2024/08/Group-22-jpg.avif" alt="" class="wp-image-139" style="object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:cb/slide -->
+
+<!-- wp:cb/slide {"className":"go-carousel-slide"} -->
+<div class="wp-block-cb-slide go-carousel-slide"><!-- wp:image {"id":136,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://gomarketing.local/wp-content/uploads/2024/08/Group-20-jpg.avif" alt="" class="wp-image-136"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:cb/slide -->
+
+<!-- wp:cb/slide {"className":"go-carousel-slide"} -->
+<div class="wp-block-cb-slide go-carousel-slide"><!-- wp:image {"id":135,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://gomarketing.local/wp-content/uploads/2024/08/Group-21-jpg.avif" alt="" class="wp-image-135"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:cb/slide --></div>
+<!-- /wp:cb/carousel --></section>
 <!-- /wp:group -->
 EOD;
